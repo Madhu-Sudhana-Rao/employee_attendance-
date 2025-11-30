0c37757438e959b07c833e89712783dc0ca8e61b
Pendyala Madhu Sudhana Rao <madhusudhanaraopendyala26@gmail.com>
2025-11-30 10:10:02 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+import authRoutes from './routes/authRoutes.js';
+import attendanceRoutes from './routes/attendanceRoutes.js';
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/attendance', attendanceRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
